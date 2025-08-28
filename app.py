8e46090f9f8e269496e7dae169f7530a1675f8b4
chekhovavalentina2-blip <chekhovavalentina2@gmail.com>
2025-08-28 09:14:03 +0300

    +

---
+++ app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return FileResponse("index.html")
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"ты ввел = ": item_id, "q": q}
+
+@app.get("/add/{a}/{b}")
+def add(a: float, b: float):
+    return {"result": a + b}
+
+@app.get("/sub/{a}/{b}")
+def sub(a: float, b: float):
+    return {"result": a - b}
+
+@app.get("/mul/{a}/{b}")
+def mul(a: float, b: float):
+    return {"result": a * b}
+
+@app.get("/div/{a}/{b}")
+def div(a: float, b: float):
+    if b == 0:
+        return {"error": "Деление на ноль"}
+    return {"result": a / b}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
