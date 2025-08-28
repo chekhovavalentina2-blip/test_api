327a6fe2b819f706ed1b9d2fa85c768cf09aa0d8
chekhovavalentina2-blip <chekhovavalentina2@gmail.com>
2025-08-28 11:16:19 +0300

    +

--- main.py
+++ main.py
@@ -141,4 +141,4 @@ def startup():
 
 
 if __name__ == "__main__":
-    uvicorn.run("task_api_json:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
